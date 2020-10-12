e6dd080607ab0fc7e2a1a865b253b87b4dacbab7
mikolaj1310 <48795346+mikolaj1310@users.noreply.github.com>
2020-10-12 04:41:50 +0100

    Added movement
    Created player and controller

---
+++ Project Paper/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Vector3 playerRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal;
+        float vertical;
+
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+    }
+}
---
+++ Project Paper/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal;
+
+        horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 135, 0);
+        }
+        else if (horizontal < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 315, 0);
+        }
+    }
+}
